c70bad2362c8e11f34a62623a57f0214ee455cd5
caofa <caofa@foxmail.com>
2017-08-08 16:47:43 +0800

    options

--- Python/machinelearning/test.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-import pandas as pd
-conn = pymysql.connect(host ='localhost',user = 'caofa',passwd = 'caofa',db = 'pythonStocks')
-df = pd.read_sql('show tables',con=conn)
-print(df)
-
-
