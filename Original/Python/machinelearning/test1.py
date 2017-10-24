a8b3fa698bfee9f7c1a58a7784bb7c78b23b3e98
caofa <caofa@foxmail.com>
2017-10-24 16:42:09 +0800

    Signed-off-by: caofa <caofa@foxmail.com>

--- Original/Python/machinelearning/test1.py
+++ Original/Python/machinelearning/test1.py
@@ -1,74 +1,67 @@
-import random,string
-import numpy as np
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+iris=datasets.load_iris()
+x_vals=np.array([x[0:3] for x in iris.data])
+y_vals=np.array([x[3] for x in iris.data])
 sess=tf.Session()
 
-n=10
-street_names=['abbey','baker','canal','donner','elm']
-street_types=['rd','st','ln','pass','ave']
-rand_zips=[random.randint(65000,65999) for i in range(n)]
-numbers=[random.randint(1,9999) for i in range(n)]
-streets=[random.choice(street_names) for i in range(n)]
-street_suffs=[random.choice(street_types) for i in range(n)]
-zips=[random.choice(rand_zips) for i in range(n)]
-full_streets=[str(x)+' '+y+' '+z for x,y,z in zip(numbers,streets,street_suffs)]
-reference_data=[list(x) for x in zip(full_streets,zips)]
+seed=2
+tf.set_random_seed(seed)
+np.random.seed(seed)
+train_indices=np.random.choice(len(x_vals),round(len(x_vals)*0.8),replace=False)
+test_indices=np.array(list(set(range(len(x_vals)))-set(train_indices)))
+x_vals_train=x_vals[train_indices]
+x_vals_test=x_vals[test_indices]
+y_vals_train=y_vals[train_indices]
+y_vals_test=y_vals[test_indices]
+def normalize_col(m):
+    col_max=m.max(axis=0)
+    col_min=m.min(axis=0)
+    return (m-col_min)/(col_max-col_min)
 
-def create_typo(s,prob=0.75):
-    if random.uniform(0,1)<prob:
-        rand_ind=random.choice(range(len(s)))
-        s_list=list(s)
-        s_list[rand_ind]=random.choice(string.ascii_lowercase)
-        s=''.join(s_list)
-    return(s)
+x_vals_train=np.nan_to_num(normalize_col(x_vals_train))
+x_vals_test=np.nan_to_num(normalize_col(x_vals_test))
+batch_size=50
+x_data=tf.placeholder(shape=[None,3],dtype=tf.float32)
+y_target=tf.placeholder(shape=[None,1],dtype=tf.float32)
+hidden_layer_nodes=5
+A1=tf.Variable(tf.random_normal(shape=[3,hidden_layer_nodes]))
+b1=tf.Variable(tf.random_normal(shape=[hidden_layer_nodes]))
+A2=tf.Variable(tf.random_normal(shape=[hidden_layer_nodes,1]))
+b2=tf.Variable(tf.random_normal(shape=[1]))
 
-typo_streets=[create_typo(x) for x in streets]
-typo_full_streets=[str(x)+' '+y+' '+z for x,y,z in zip(numbers,typo_streets,street_suffs)]
-test_data=[list(x) for x in zip(typo_full_streets,zips)]
+hidden_output=tf.nn.relu(tf.add(tf.matmul(x_data,A1),b1))
+final_output=tf.nn.relu(tf.add(tf.matmul(hidden_output,A2),b2))
+loss=tf.reduce_mean(tf.square(y_target-final_output))
+my_opt=tf.train.GradientDescentOptimizer(0.005)
+train_step=my_opt.minimize(loss)
+init=tf.global_variables_initializer()
+sess.run(init)
+loss_vec=[]
+test_loss=[]
+for i in range(500):
+    rand_index=np.random.choice(len(x_vals_train),size=batch_size)
+    rand_x=x_vals_train[rand_index]
+    rand_y=np.transpose([y_vals_train[rand_index]])
+    sess.run(train_step,feed_dict={x_data:rand_x,y_target:rand_y})
+    temp_loss=sess.run(loss,feed_dict={x_data:rand_x,y_target:rand_y})
+    loss_vec.append(np.sqrt(temp_loss))
+    test_temp_loss=sess.run(loss,feed_dict={x_data:x_vals_test,\
+                    y_target:np.transpose([y_vals_test])})
+    test_loss.append(np.sqrt(test_temp_loss))
+    if (i+1)%50==0:
+        print('Generation:'+str(i+1)+'.Loss='+str(temp_loss))
 
-test_address=tf.sparse_placeholder(dtype=tf.string)
-test_zip=tf.placeholder(shape=[None,1],dtype=tf.float32)
-ref_address=tf.sparse_placeholder(dtype=tf.string)
-ref_zip=tf.placeholder(shape=[None,n],dtype=tf.float32)
-
-zip_dist=tf.square(tf.subtract(ref_zip,test_zip))
-address_dist=tf.edit_distance(test_address,ref_address,normalize=True)
-zip_max=tf.gather(tf.squeeze(zip_dist),tf.argmax(zip_dist,1))
-zip_min=tf.gather(tf.squeeze(zip_dist),tf.argmax(zip_dist,1))
-zip_sim=tf.div(tf.subtract(zip_max,zip_dist),tf.subtract(zip_max,zip_min))
-address_sim=tf.subtract(1.,address_dist)
-address_weight=0.5
-zip_weight=1.-address_weight
-weighted_sim=tf.add(tf.transpose(tf.multiply(address_weight,address_sim)),tf.multiply(zip_weight,zip_sim))
-top_match_index=tf.argmax(weighted_sim,1)
-
-def sparse_from_word_vec(word_vec):
-    num_words=len(word_vec)
-    indices=[[xi,0,yi] for xi,x in enumerate(word_vec) for yi,y in enumerate(x)]
-    chars=list(''.join(word_vec))
-    return (tf.SparseTensorValue(indices,chars,[num_words,1,1]))
-
-reference_addresses=[x[0] for x in reference_data]
-reference_zips=np.array([[x[1] for x in reference_data]])
-sparse_ref_set=sparse_from_word_vec(reference)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plt.plot(loss_vec,'k-',label='Train Loss')
+plt.plot(test_loss,'r--',label='Test Loss')
+plt.title('Loss (MSE) per Generation')
+plt.xlabel('Generation')
+plt.ylabel('Loss')
+plt.legend(loc='upper right')
+plt.show()
 
 
 
