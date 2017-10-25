b4e99f3c2a547f2c3df030fb4233bc1175cea4bc
caofa <caofa@foxmail.com>
2017-10-25 16:54:50 +0800

    Signed-off-by: caofa <caofa@foxmail.com>

--- JuXinOffice/optionShort.m
+++ JuXinOffice/optionShort.m
@@ -718,9 +718,216 @@
     
 
 
-% loop for final version-absolute short one time ;
+% % loop for final version-absolute short one time ;
+% stepShortS=[1,2,3]; % gas between current value and void value;
+% holdDaysS=[10,13,15,18,20,23,25,28,30,35]; % trading days from "short" to expire date;
+% stopRatioS=[1.1,1.3,1.5,1.7,2.0];
+% dateFrom='2014/5/1';
+% dateFrom=year(datenum(dateFrom))*100+month(datenum(dateFrom));
+% w=windmatlab;
+% loops=ceil((today-datenum(2015,2,9))/29);
+% Year=2015;
+% Month=1;
+% monthTem=1;
+% Day=28;
+% Data=[];% store data for all options;
+% ReAll={};
+% ReMean=[];
+% monthAll={};
+% paras={};
+% for j=1:loops
+%     if monthTem==12
+%         Year=Year+1;
+%     end
+%     Month=Month+1;
+%     monthTem=mod(Month,12);
+%     if monthTem==0
+%         monthTem=12;
+%     end
+%     Date=datenum(Year,monthTem,Day);
+%     if Date >today
+%         Date=today;
+%     end    
+%     parameters=['date=',datestr(Date,'yyyy-mm-dd'),';','us_code=510050.SH;option_var=全部;',...
+%         'field=option_code,strike_price,month,call_put,first_tradedate,last_tradedate,option_name'];
+%     data=w.wset('optionchain',parameters);
+%     Data=[Data;data];
+%     if Date==today
+%         break;
+%     end       
+% end
+% options=Data(:,1);
+% [options,indTem]=unique(options);
+% Data=Data(indTem,:);
+% prices=cell2mat(Data(:,2));
+% indTem=mod(prices,0.05)<0.00000001;
+% prices=prices(indTem);
+% months=cell2mat(Data(indTem,3));
+% calls=Data(indTem,4);
+% starts=Data(indTem,5);
+% ends=Data(indTem,6);    
+% options=options(indTem);
+% tem=ismember(calls,'认购');
+% optionsB=options(tem);
+% pricesB=prices(tem);
+% monthsB=months(tem);
+% prices=prices(~tem);
+% months=months(tem);
+% calls=calls(~tem);
+% starts=starts(~tem);
+% ends=ends(~tem);    
+% options=options(~tem);
+% 
+% j123=0;
+% jall=string(length(stepShortS)*length(holdDaysS)*length(stopRatioS));
+% for j1=1:length(stepShortS)
+%     for j2=1:length(holdDaysS)
+%         for j3=1:length(stopRatioS)
+%             try
+%                 j123=j123+1;
+%                 display([j1,j2,j3]);
+%                 display(strcat(string(j123),'/',jall));            
+%                 stepShort=stepShortS(j1); % gas between current value and void value;
+%                 holdDays=holdDaysS(j2); % trading days from "short" to expire date;
+%                 stopRatio=stopRatioS(j3);
+%                 Re=[];
+%                 ReC=[];
+%                 records=[];
+%                 recordOptions={};
+%                 optionsRec={};
+%                 Comm=[];%for commission of all;
+%                 etfMonth={};
+%                 UniMonth=unique(months);
+%                 loops=length(UniMonth);
+% 
+%                 for i=1:loops  
+%                     if Year*100+month(today)<=UniMonth(i)
+%                         continue;
+%                     end
+%                     if dateFrom>UniMonth(i)
+%                         UniMonth(i)
+%                         continue;
+%                     end
+%                     indT=months==UniMonth(i);
+%                     optionT=options(indT);
+%                     priceT=prices(indT);
+%                     startT=starts(indT);
+%                     endT=ends(indT);
+%                     [priceT,tem]=unique(priceT);
+%                     optionT=optionT(tem);
+%                     startT=startT(tem);
+%                     endT=endT(tem); 
+%                     tem=datenum(endT)-datenum(startT)>5;
+%                     priceT=priceT(tem);
+%                     optionT=optionT(tem);
+%                     startT=startT(tem);
+%                     endT=endT(tem); 
+% 
+%                     tradeDays=w.tdays(datenum(endT(1))-50,endT(1));
+%                     tradeDay=tradeDays(end-holdDays);
+%                     etfP=w.wss('510050.SH','close',['tradeDate=',tradeDay{1}],'priceAdj=U','cycle=D');
+%                     difftem=abs(priceT+stepShort*0.05-etfP);
+%                     [~,tem]=sort(difftem);
+%                     optioni=optionT(tem(1));starti=startT(tem(1));endi=endT(tem(1));pricei=priceT(tem(1));
+%                     if min(difftem)>0.05 || datenum(starti)>datenum(tradeDay)
+%                         continue;
+%                     end
+% 
+%                     tem_1=w.wss(optioni,'open',['tradeDate=',tradeDay{1}],'priceAdj=U','cycle=D');
+%                     etf50_1=w.wss('510050.SH','open',['tradeDate=',tradeDay{1}],'priceAdj=U','cycle=D');
+%                     [priceTem,~,~,dayTem]=w.wsd(optioni,'close',tradeDay{1},endi,'priceAdj=U');
+%                     priceStop=priceTem(end);
+%                     hands1=floor(0.1/(priceTem(1)*(stopRatio-1)));
+%                     hands2=floor(10000/commission(pricei,tem_1,etf50_1));
+%                     ReTem=((priceTem(1)-priceStop)*10000-7.2)*min(hands1,hands2);
+%                     Re=[Re;ReTem];
+%                     tem=w.wsd(optioni,'open,high,low',tradeDay{1},endi,'priceAdj=U');
+%                     openTem=tem(:,1);highTem=tem(:,2);lowTem=tem(:,3);closeTem=priceTem;
+%                     for i2=2:length(priceTem)
+%                         if highTem(i2)>=priceTem(1)*stopRatio
+%                             priceStop=max(openTem(i2),priceTem(1)*stopRatio);
+%                             ReTem=((priceTem(1)-priceStop)*10000-7.2)*min(hands1,hands2);
+%                             tem=(monthsB==UniMonth(i))&(pricesB==pricei-2*stepShort*0.05);
+%                             optionNew=optionsB(tem);
+%                             if ~isempty(optionNew)
+%                                 tem=w.wsd(optionNew,'close,open,high',datestr(dayTem(i2),'yyyy/mm/dd'),endi,'priceAdj=U');
+%                                 if  max(max(tem))/tem(1,1)>=stopRatio
+%                                     indT=find(tem(:,3)>tem(1,1)*stopRatio,1);
+%                                     if tem(indT,2)>tem(1,1)*stopRatio
+%                                         stopT=tem(indT,2);
+%                                     else
+%                                         stopT=tem(1,1)*stopRatio;
+%                                     end
+%                                     Re(end)=ReTem+((tem(1,1)-stopT)*10000-7.2)*min(hands1,hands2); 
+%                                 else
+%                                     Re(end)=ReTem+((tem(1,1)-tem(end,1))*10000-7.2)*min(hands1,hands2);     
+%                                 end
+%                             else
+%                                 tem=(monthsB==UniMonth(i))&(pricesB<pricei-stepShort*0.05);
+%                                 pricesBtem=pricesB(tem);optionsBtem=optionsB(tem);
+%                                 [~,tem]=sort(pricesBtem);
+%                                 if isempty(tem)
+%                                     Re(end)=ReTem;
+%                                     display('No other options');
+%                                 else
+%                                     optionNew=optionsBtem(tem(1));
+%                                     tem=w.wsd(optionNew,'close,open,high',datestr(dayTem(i2),'yyyy/mm/dd'),endi,'priceAdj=U');
+%                                     if max(max(tem))/tem(1,1)>=stopRatio
+%                                         indT=find(tem(:,3)>tem(1,1)*stopRatio,1);
+%                                         if tem(indT,2)>tem(1,1)*stopRatio
+%                                             stopT=tem(indT,2);
+%                                         else
+%                                             stopT=tem(1,1)*stopRatio;
+%                                         end
+%                                         Re(end)=ReTem+((tem(1,1)-stopT)*10000-7.2)*min(hands1,hands2); 
+%                                     else
+%                                         Re(end)=ReTem+((tem(1,1)-tem(end,1))*10000-7.2)*min(hands1,hands2);     
+%                                     end
+%                                     display('use reverse options');
+%                                 end
+%                             end
+%                             break;
+%                         end
+%                     end
+%                     etfMonth=[etfMonth;tradeDay{1}];
+%                 end
+%                 tem=~isnan(Re);
+%                 Re=Re(tem);
+%                 etfMonth=etfMonth(tem);
+%                 ReAll=[ReAll,Re];
+%                 ReMean=[ReMean,mean(Re)/std(Re)];
+%                 monthAll=[monthAll,{etfMonth}];
+%                 paras=[paras,[j1,j2,j3]];
+%             end
+%         end
+%     end
+% end
+% [~,tem]=sort(ReMean);
+% Re=ReAll{tem(end)};
+% etfMonth=monthAll{tem(end)};
+% para=paras{tem(end)};
+% figure;
+% winRatio=sum(Re>0)/length(Re);
+% ReCS=cumsum(Re)+10000;
+% plot(ReCS);
+% grid on;
+% Lre=length(Re);
+% step=max(floor(Lre/10),1);
+% set(gca,'xtick',1:step:Lre);
+% set(gca,'xticklabel',etfMonth(1:step:Lre),'XTickLabelRotation',60);
+% maxDown=0;
+% ReCS=[0;ReCS];
+% for i=2:Lre
+%     tem=max(ReCS(1:i))-ReCS(i);
+%     if tem>maxDown
+%         maxDown=tem;
+%     end
+% end
+% title(sprintf('Step:%d,holdDays:%d,stopRatio:%.2f;\n 年化收益估算：%.2f%%;最大回撤：%.2f%%;胜率：%.2f%%.',stepShortS(para(1)),holdDaysS(para(2)),stopRatioS(para(3)),100*((ReCS(end)/10000)^(1/((datenum(etfMonth(end))-datenum(etfMonth(1)))/360))-1), maxDown/100,winRatio*100));
+
+% loop for use next open short;
 stepShortS=[1,2,3]; % gas between current value and void value;
-holdDaysS=[21]; % trading days from "short" to expire date;
+holdDaysS=[10,13,15,18,20,23,25,28,30,35]; % trading days from "short" to expire date;
 stopRatioS=[1.1,1.3,1.5,1.7,2.0];
 dateFrom='2014/5/1';
 dateFrom=year(datenum(dateFrom))*100+month(datenum(dateFrom));
@@ -850,11 +1057,17 @@ for j1=1:length(stepShortS)
                             tem=(monthsB==UniMonth(i))&(pricesB==pricei-2*stepShort*0.05);
                             optionNew=optionsB(tem);
                             if ~isempty(optionNew)
-                                tem=w.wsd(optionNew,'close',datestr(dayTem(i2),'yyyy/mm/dd'),endi,'priceAdj=U');
-                                if 0% max(tem)/tem(1)>=stopRatio
-                                    Re(end)=ReTem+((tem(1)-tem(1)*stopRatio)*10000-7.2)*min(hands1,hands2); 
+                                tem=w.wsd(optionNew,'close,open,high',datestr(dayTem(i2),'yyyy/mm/dd'),endi,'priceAdj=U');
+                                if  max(tem(:,3))/tem(1,1)>=stopRatio
+                                    indT=find(tem(:,3)>=tem(1,1)*stopRatio,1);
+                                    if tem(indT,2)>tem(1,1)*stopRatio
+                                        stopT=tem(indT,2);
+                                    else
+                                        stopT=tem(1,1)*stopRatio;
+                                    end
+                                    Re(end)=ReTem+((tem(1,1)-stopT)*10000-7.2)*min(hands1,hands2); 
                                 else
-                                    Re(end)=ReTem+((tem(1)-tem(end))*10000-7.2)*min(hands1,hands2);     
+                                    Re(end)=ReTem+((tem(1,1)-tem(end,1))*10000-7.2)*min(hands1,hands2);     
                                 end
                             else
                                 tem=(monthsB==UniMonth(i))&(pricesB<pricei-stepShort*0.05);
@@ -865,11 +1078,17 @@ for j1=1:length(stepShortS)
                                     display('No other options');
                                 else
                                     optionNew=optionsBtem(tem(1));
-                                    tem=w.wsd(optionNew,'close',datestr(dayTem(i2),'yyyy/mm/dd'),endi,'priceAdj=U');
-                                    if 0%max(tem)/tem(1)>=stopRatio
-                                        Re(end)=ReTem+((tem(1)-tem(1)*stopRatio)*10000-7.2)*min(hands1,hands2); 
+                                    tem=w.wsd(optionNew,'close,open,high',datestr(dayTem(i2),'yyyy/mm/dd'),endi,'priceAdj=U');
+                                    if max(max(tem))/tem(1,1)>=stopRatio
+                                        indT=find(tem(:,3)>=tem(1,1)*stopRatio,1);
+                                        if tem(indT,2)>tem(1,1)*stopRatio
+                                            stopT=tem(indT,2);
+                                        else
+                                            stopT=tem(1,1)*stopRatio;
+                                        end
+                                        Re(end)=ReTem+((tem(1,1)-stopT)*10000-7.2)*min(hands1,hands2); 
                                     else
-                                        Re(end)=ReTem+((tem(1)-tem(end))*10000-7.2)*min(hands1,hands2);     
+                                        Re(end)=ReTem+((tem(1,1)-tem(end,1))*10000-7.2)*min(hands1,hands2);     
                                     end
                                     display('use reverse options');
                                 end
@@ -912,8 +1131,6 @@ for i=2:Lre
     end
 end
 title(sprintf('Step:%d,holdDays:%d,stopRatio:%.2f;\n 年化收益估算：%.2f%%;最大回撤：%.2f%%;胜率：%.2f%%.',stepShortS(para(1)),holdDaysS(para(2)),stopRatioS(para(3)),100*((ReCS(end)/10000)^(1/((datenum(etfMonth(end))-datenum(etfMonth(1)))/360))-1), maxDown/100,winRatio*100));
-
-    
     
     
     
--- Original/Python/machinelearning/test1.py
+++ Original/Python/machinelearning/test1.py
@@ -1,67 +1,76 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import datasets
-
-iris=datasets.load_iris()
-x_vals=np.array([x[0:3] for x in iris.data])
-y_vals=np.array([x[3] for x in iris.data])
 sess=tf.Session()
 
-seed=2
-tf.set_random_seed(seed)
-np.random.seed(seed)
-train_indices=np.random.choice(len(x_vals),round(len(x_vals)*0.8),replace=False)
-test_indices=np.array(list(set(range(len(x_vals)))-set(train_indices)))
-x_vals_train=x_vals[train_indices]
-x_vals_test=x_vals[test_indices]
-y_vals_train=y_vals[train_indices]
-y_vals_test=y_vals[test_indices]
-def normalize_col(m):
-    col_max=m.max(axis=0)
-    col_min=m.min(axis=0)
-    return (m-col_min)/(col_max-col_min)
+data_size=[10,10]
+data_2d=np.random.normal(size=data_size)
+x_input_2d=tf.placeholder(dtype=tf.float32,shape=data_size)
 
-x_vals_train=np.nan_to_num(normalize_col(x_vals_train))
-x_vals_test=np.nan_to_num(normalize_col(x_vals_test))
-batch_size=50
-x_data=tf.placeholder(shape=[None,3],dtype=tf.float32)
-y_target=tf.placeholder(shape=[None,1],dtype=tf.float32)
-hidden_layer_nodes=5
-A1=tf.Variable(tf.random_normal(shape=[3,hidden_layer_nodes]))
-b1=tf.Variable(tf.random_normal(shape=[hidden_layer_nodes]))
-A2=tf.Variable(tf.random_normal(shape=[hidden_layer_nodes,1]))
-b2=tf.Variable(tf.random_normal(shape=[1]))
+def conv_layer_2d(input_2d,my_filter):
+    input_3d=tf.expand_dims(input_2d,0)
+    input_4d=tf.expand_dims(input_3d,3)
+    convolution_output=tf.nn.conv2d(input_4d,filter=my_filter,\
+            strides=[1,2,2,1],padding='VALID')
+    conv_output_2d=tf.squeeze(convolution_output)
+    return(conv_output_2d)
+my_filter=tf.Variable(tf.random_normal(shape=[2,2,1,1]))
+my_convolution_output=conv_layer_2d(x_input_2d,my_filter)
+def activation(input_2d):
+    return(tf.nn.relu(input_2d))
+my_activation_output=activation(my_convolution_output)
+def max_pool(input_2d,width,height):
+    input_3d=tf.expand_dims(input_2d,0)
+    input_4d=tf.expand_dims(input_3d,3)
+    pool_output=tf.nn.max_pool(input_4d,ksize=[1,height,width,1],\
+                               strides=[1,1,1,1],padding='VALID')
+    pool_output_2d=tf.squeeze(pool_output)
+    return(pool_output_2d)
+my_maxpool_output=max_pool(my_activation_output,width=2,height=2)
 
-hidden_output=tf.nn.relu(tf.add(tf.matmul(x_data,A1),b1))
-final_output=tf.nn.relu(tf.add(tf.matmul(hidden_output,A2),b2))
-loss=tf.reduce_mean(tf.square(y_target-final_output))
-my_opt=tf.train.GradientDescentOptimizer(0.005)
-train_step=my_opt.minimize(loss)
+def fully_connected(input_layer,num_outputs):
+    flat_input=tf.reshape(input_layer,[-1])
+    weight_shape=tf.squeeze(tf.stack([tf.shape(flat_input),[num_outputs]]))
+    weight=tf.random_normal(weight_shape,stddev=0.1)
+    bias=tf.random_normal(shape=[num_outputs])
+    input_2d=tf.expand_dims(flat_input,0)
+    full_output=tf.add(tf.matmul(input_2d,weight),bias)
+    full_output_2d=tf.squeeze(full_output)
+    return(full_output_2d)
+my_full_output=fully_connected(my_maxpool_output,5)
 init=tf.global_variables_initializer()
 sess.run(init)
-loss_vec=[]
-test_loss=[]
-for i in range(500):
-    rand_index=np.random.choice(len(x_vals_train),size=batch_size)
-    rand_x=x_vals_train[rand_index]
-    rand_y=np.transpose([y_vals_train[rand_index]])
-    sess.run(train_step,feed_dict={x_data:rand_x,y_target:rand_y})
-    temp_loss=sess.run(loss,feed_dict={x_data:rand_x,y_target:rand_y})
-    loss_vec.append(np.sqrt(temp_loss))
-    test_temp_loss=sess.run(loss,feed_dict={x_data:x_vals_test,\
-                    y_target:np.transpose([y_vals_test])})
-    test_loss.append(np.sqrt(test_temp_loss))
-    if (i+1)%50==0:
-        print('Generation:'+str(i+1)+'.Loss='+str(temp_loss))
+feed_dict={x_input_2d:data_2d}
 
-plt.plot(loss_vec,'k-',label='Train Loss')
-plt.plot(test_loss,'r--',label='Test Loss')
-plt.title('Loss (MSE) per Generation')
-plt.xlabel('Generation')
-plt.ylabel('Loss')
-plt.legend(loc='upper right')
-plt.show()
+print('Input = [10 X 10] array')
+print('2X2 Convolution, stride size=[2X2],results in the [5X5] array:')
+print(sess.run(my_convolution_output,feed_dict=feed_dict))
+print('Input=the above [5X5] arrray')
+print('ReLU element wise returns the [5X5] array:')
+print(sess.run(my_activation_output,feed_dict=feed_dict))
+print('Input=the above [5X5] array')
+print('MaxPool,stride size=[1X1],resutls in the [4X4] array:')
+print(sess.run(my_maxpool_output,feed_dict=feed_dict))
+print('Input = the above [4X4] array')
+print('Fully connected layer on all four rows with five outputs:')
+print(sess.run(my_full_output,feed_dict=feed_dict))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
