6477d60ac515ece89415229757f6dada2d68d29a
caofa <caofa@foxmail.com>
2018-03-06 09:21:29 +0800

    spark

---
+++ Original/Files/慧与大数据/AI课程/二期课程/算法/code/pureNN.py
@@ -0,0 +1,114 @@
+# import numpy as np
+# import pdb
+#
+# x=np.random.randn(5,5)
+# y = np.array([[1, 0, 0, 0],[0,1,0,0],[0,0,0,1],[0,1,0,0],[0,0,0,1]]).T
+# theta1 = np.random.randn(10,5)
+# theta2=np.random.randn(10,10)
+# theta3=np.random.randn(4,10)
+#
+# for i in range(1000):
+#     a1=x
+#     a2 = 1/(1+np.exp(-np.dot(theta1, a1)))
+#     a3= 1/(1+np.exp(-np.dot(theta2, a2)))
+#     a4= 1/(1+np.exp(-np.dot(theta3, a3)))
+#     delta4=a4-y
+#     # pdb.set_trace()
+#     delta3=np.dot(theta3.T,delta4)*(a3*(1-a3))
+#     delta2=np.dot(theta2.T,delta3)*(a2*(1-a2))
+#     theta1 -= np.dot(delta2,a1.T)
+#     theta2 -= np.dot(delta3,a2.T)
+#     theta3 -= np.dot(delta4,a3.T)
+#
+# print("the training result is:")
+# print(a4)
+
+# import numpy as np
+#
+# def NN(x,y,hiddenLayers):
+#     netWork=np.concatenate([[x.shape[0]],hiddenLayers,[y.shape[0]]])
+#     Lnet=len(netWork)
+#     theta=[]
+#     for i in range(Lnet-1):
+#         theta.append(np.random.randn(netWork[i+1],netWork[i]))
+#
+#     for i in range(1000):
+#         a=[x]
+#         for i2 in range(1,Lnet):
+#             a.append(1 / (1 + np.exp(-np.dot(theta[i2-1], a[i2-1]))))
+#
+#         delta=[a[-1]-y]
+#         for i2 in range(1,Lnet):
+#             delta.insert(0,np.dot(theta[-i2].T, delta[-i2]) * (a[-i2-1] * (1 - a[-i2-1])))
+#         for i2 in range(0,Lnet-1):
+#             theta[i2]-=np.dot(delta[i2+1],a[i2].T)
+#
+#     print("the training result is:")
+#     print(a[-1])
+#
+# x=np.random.randn(5,5)
+# y = np.array([[1, 0, 0, 0],[0,1,0,0],[0,0,0,1],[0,1,0,0],[0,0,0,1]]).T
+# NN(x,y,[10,20,12])
+
+
+import pandas as pd
+from sklearn import preprocessing
+import pdb
+
+def NN(x,y,hiddenLayers,learnRatio):
+    netWork=np.concatenate([[x.shape[0]],hiddenLayers,[y.shape[0]]])
+    Lnet=len(netWork)
+    theta=[]
+    for i in range(Lnet-1):
+        theta.append(np.random.randn(netWork[i+1],netWork[i]))
+
+    for i in range(10000):
+        a=[x]
+        for i2 in range(1,Lnet):
+            a.append(1 / (1 + np.exp(-np.dot(theta[i2-1], a[i2-1]))))
+
+        delta=[a[-1]-y]
+        for i2 in range(1,Lnet):
+            delta.insert(0,np.dot(theta[-i2].T, delta[-i2]) * (a[-i2-1] * (1 - a[-i2-1])))
+        for i2 in range(0,Lnet-1):
+            theta[i2]-=learnRatio*np.dot(delta[i2+1],a[i2].T)/x.shape[0]
+
+    print("the training result is:")
+    tmp1=np.argmax(y,axis=0)
+    tmp2=np.argmax(a[-1],axis=0)
+    print(sum(tmp1==tmp2)/y.shape[1])
+
+data=pd.read_csv('C:\\Users\\Administrator\\Desktop\\iris.txt',sep=',',header=None)
+x=data.iloc[:,:4].values
+x=preprocessing.minmax_scale(x)
+x=x.T
+y=data.iloc[:,4].values
+# pdb.set_trace()
+# NN(x,y,[10,20,12],learnRatio=0.1)
+# print(data)
+Y=[]
+for i in y:
+    if i=='Iris-setosa':
+        Y.append([1,0,0])
+    elif i=='Iris-versicolor':
+        Y.append([0,1,0])
+    else:
+        Y.append([0,0,1])
+Y=np.array(Y).T
+NN(x,Y,[8,8],learnRatio=0.3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
