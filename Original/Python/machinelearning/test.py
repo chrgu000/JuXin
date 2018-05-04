d2cef7a19b726b9c583a1aca1ab9697179a34db6
caofa <caofa@foxmail.com>
2018-05-04 13:41:03 +0800

    new

--- Original/Python/machinelearning/test.py
+++ Original/Python/machinelearning/test.py
@@ -1,3 +1,59 @@
+import hashlib,json
+from time import time
+from hashlib import sha256
+from uuid import uuid4
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain=[]
+        self.current_transactions=[]
+        self.new_block(previous_hash=1,proof=100)
+                
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        block={
+            'index':len(self.chain)+1,
+            'timestamp':time(),
+            'transactions':self.current_transactions,
+            'proof':proof,
+            'previous_hash':previous_hash or self.hash(self.chain[-1]),              
+            }
+        self.current_transactions=[]
+        self.chain.append(block)
+        return block
+        
+    def new_transactions(self,sender,recipient,amount):
+        # Adds a new transaction to the list of transactions
+        '''
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient:<str> Address of the Recipient
+        :param amount:<int> Amount
+        :return <int> The index of the Block that will hold this transaction
+        '''
+        self.current_transactions.append({
+                'sender':sender,
+                'recipient':recipient,
+                'amount':amount
+                })
+        return self.last_block['index']+1
+    
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        block_string=json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+    
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        return self.chain[-1]
+
+x=5
+y=0
+while sha256(f'{x*y}'.encode()).hexdigest()[-1]!='0':
+    y+=1
+print(f'The solution is y={y}')
 
 
 
